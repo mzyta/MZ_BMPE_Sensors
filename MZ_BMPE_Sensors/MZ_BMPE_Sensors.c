f991071ab6bcfa0ad8aa0ddb7e3b40715d930033
Mateusz Żyta <mateusz@mzyta.pl>
2022-11-15 21:27:24 +0100

    Creating device struct, error enum, init function

--- MZ_BMPE_Sensors/MZ_BMPE_Sensors.c
+++ MZ_BMPE_Sensors/MZ_BMPE_Sensors.c
@@ -6,3 +6,56 @@
  */
 
 
+#include "MZ_BMPE_Sensors.h"
+
+static uint8_t DeviceCount = 0;
+
+
+MZ_BMPE_Errors_t MZ_BMPE_DeviceInit(MZ_BMPE_Device_t *DevicePtr, uint8_t DeviceId, uint8_t DeviceAddress, uint8_t DeviceType, uint8_t CommunicationProtocol)
+{
+	if(DevicePtr == 0)
+	{
+		return BMPE_DEVICE_POINTER_ERROR;
+	}
+	else
+	{
+
+		//Checking device address and writing it to device pointer
+		if(DeviceAddress == 0)
+		{
+			return BMPE_INIT_ERROR;
+		}
+		else
+		{
+			DevicePtr->DeviceAddress = DeviceAddress;
+		}
+
+		//Checking device type and writing it to device pointer
+		if(DeviceType == 0)
+		{
+			return BMPE_INIT_ERROR;
+		}
+		else
+		{
+			DevicePtr->DeviceType = DeviceType;
+		}
+
+		//Checking communication protocol and writing it to device pointer
+		if(CommunicationProtocol == 0)
+		{
+			return BMPE_INIT_ERROR;
+		}
+		else
+		{
+			DevicePtr->CommunicationProtocol = CommunicationProtocol;
+		}
+
+		//Creating device Id
+		DeviceCount += 1;
+		DevicePtr->DeviceId = DeviceId;
+
+	}
+
+	return BMPE_OK;
+
+}
--- MZ_BMPE_Sensors/MZ_BMPE_Sensors.h
+++ MZ_BMPE_Sensors/MZ_BMPE_Sensors.h
@@ -8,6 +8,41 @@
 #ifndef MZ_BMPE_SENSORS_H_
 #define MZ_BMPE_SENSORS_H_
 
+#include <stdint.h>
 
+//#include "main.h"
+
+typedef struct{
+	//Variable for future solutions
+	uint8_t DeviceId;
+
+	//Device address if it's need to communication protocol
+	uint8_t DeviceAddress;
+
+	/*Sensor type
+	 * 1= BMP180
+	 * 2= BMP280
+	 * 3= BME280
+	 */
+	uint8_t DeviceType;
+
+	/*Communication protocol for device
+	 * 1= I2C
+	 * 2= SDI
+	 */
+	uint8_t CommunicationProtocol;
+
+} MZ_BMPE_Device_t;
+
+typedef enum{
+	BMPE_OK = 0,
+	BMPE_DEVICE_POINTER_ERROR = 1,
+	BMPE_INIT_ERROR = 2,
+	BMPE_WRONG_COMMUNICATION_PROTOCOL = 3,
+	BMPE_COMMUNICATION_PROBLEM = 4
+}MZ_BMPE_Errors_t;
+
+
+MZ_BMPE_Errors_t MZ_BMPE_DeviceInit(MZ_BMPE_Device_t *DevicePtr, uint8_t DeviceId, uint8_t DeviceAddress, uint8_t DeviceType, uint8_t CommunicationProtocol);
 
 #endif /* MZ_BMPE_SENSORS_H_ */
