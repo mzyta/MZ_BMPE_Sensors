78f00c2dc3106457113fc7f0b5c8dd95a77ad3de
Mateusz Żyta <mateusz@mzyta.pl>
2022-11-15 20:44:36 +0100

    Creating files and repository with gitignore

---
+++ MZ_BMPE_Sensors/MZ_BMPE_Sensors.c
@@ -0,0 +1,8 @@
+/*
+ * MZ_BMPE_Sensors.c
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: zytam
+ */
+
+
---
+++ MZ_BMPE_Sensors/MZ_BMPE_Sensors.h
@@ -0,0 +1,13 @@
+/*
+ * MZ_BMPE_Sensors.h
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: zytam
+ */
+
+#ifndef MZ_BMPE_SENSORS_H_
+#define MZ_BMPE_SENSORS_H_
+
+
+
+#endif /* MZ_BMPE_SENSORS_H_ */
